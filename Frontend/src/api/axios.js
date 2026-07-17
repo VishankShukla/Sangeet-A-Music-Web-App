7ce92dbb3992f706c78d334d1ad09f2de445e069
Vishank Shukla <vishankshukla51@gmail.com>
2026-07-17 14:53:10 +0530

    Update BASE_URL to production server

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Change this if your backend runs on a different host/port
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://sangeet-ixdy.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
